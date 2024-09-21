db736b893a48ac567a65eeaf72b113fe9792576a
maulidaarahmah <joohyunbae116@gmail.com>
2024-09-21 12:17:47 +0800

    Add files via upload

---
+++ Variable01.java
@@ -0,0 +1,25 @@
+public class Variable01 {
+    public static void main(String[] args) {
+        // deklarasi variable
+        String nama; 
+        String huruf;
+        int umur;
+        String prodi;
+
+        // input nilai
+        nama = "Moli";
+        umur = 18;
+        prodi = "ilmu komputer";
+
+        // output ke layar
+        // System.out.println(huruf + nama);
+        // atau
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur: " + umur);
+        System.out.println("Prodi: " + prodi);
+
+        // tugas
+        // kompilasi perintah java
+        // run : java
+    }
+}
